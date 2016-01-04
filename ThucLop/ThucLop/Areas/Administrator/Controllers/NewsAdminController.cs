8607b1ee2fe9d01a6d0bcb4565c34a0aa0180e56
Nam Tran Hoang <namth@simaxsv5.simax5.com.vn>
2016-01-04 03:01:00 -0800

    UPDATE

---
+++ ThucLop/ThucLop/Areas/Administrator/Controllers/NewsAdminController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ThucLop.Areas.Administrator.Controllers
+{
+    public class NewsAdminController : Controller
+    {
+        //
+        // GET: /Administrator/NewsAdmin/
+        public ActionResult ViewNews()
+        {
+            return View();
+        }
+        public ActionResult _CreateNews()
+        {
+            return View();
+        }
+        public ActionResult _EditNews()
+        {
+            return View();
+        }
+        public ActionResult _DeleteNews()
+        {
+            return View();
+        }
+	}
+}
