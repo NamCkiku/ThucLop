f32b8475e25f6ba27782c97018e6e695bb64cdbf
Nam Tran Hoang <namth@simaxsv5.simax5.com.vn>
2016-01-06 01:52:03 -0800

    Update

---
+++ ThucLop/ThucLop/Areas/Administrator/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ThucLop.Areas.Administrator.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Administrator/Account/
+        public ActionResult ViewAccount()
+        {
+            return View();
+        }
+	}
+}
